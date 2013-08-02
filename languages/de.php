90d016d41286f247324870c3b10f0c25e32fe11e
cmb69 <cmbecker69@gmx.de>
2013-08-02 22:58:54 +0000

    revised language files

--- languages/de.php
+++ languages/de.php
@@ -6,4 +6,13 @@ $plugin_tx['moved']['title_gone']="Seite existiert nicht mehr";
 $plugin_tx['moved']['desc_notfound']="Pardon. Die Seite \"{{URL}}\", nach der Sie suchen, existiert nicht.";
 $plugin_tx['moved']['desc_gone']="Pardon. Die Seite \"{{URL}}\", nach der Sie suchen, wurde entfernt.";
 
+$plugin_tx['moved']['syscheck']="System-Prüfung";
+$plugin_tx['moved']['syscheck_phpversion']="PHP-Version &ge; %s";
+$plugin_tx['moved']['syscheck_extension']="Erweiterung \"%s\" geladen";
+$plugin_tx['moved']['syscheck_encoding']="UTF-8 Kodierung eingestellt";
+$plugin_tx['moved']['syscheck_magic_quotes']="\"Magic_quotes_runtime\" aus";
+$plugin_tx['moved']['syscheck_writable']="Ordner \"%s\" beschreibbar";
+
+$plugin_tx['moved']['about']="Über Moved_XH";
+
 ?>
