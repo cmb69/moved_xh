ad057ef2e0773eaf78e824a9d3cdab1083797f09
Christoph M. Becker <cmbecker69@gmx.de>
2017-05-04 15:52:45 +0200

    Improve admin view

--- views/admin.php
+++ views/admin.php
@@ -1,11 +1,13 @@
-<!-- Moved_XH: admin -->
-<div class="pluginedit">
+<div class="moved_main">
+    <h1>Moved</h1>
     <form action="<?=$this->actionUrl()?>" method="post">
-        <div class="plugineditcaption">Moved</div>
         <input type="hidden" name="admin" value="plugin_main">
         <input type="hidden" name="action" value="plugin_textsave">
-        <textarea class="plugintextarea" name="plugin_text" cols="80" rows="25"><?=$this->contents()?></textarea>
-        <br>
-        <input type="submit" class="submit" value="<?=$this->saveLabel()?>">
+        <p>
+            <textarea class="plugintextarea" name="plugin_text" cols="80" rows="25"><?=$this->contents()?></textarea>
+        </p>
+        <p>
+            <button><?=$this->saveLabel()?></button>
+        </p>
     </form>
 </div>
