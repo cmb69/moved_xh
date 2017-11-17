9a9607fd4c18d5b39216e02a2c6d135c81590f6a
Christoph M. Becker <cmbecker69@gmx.de>
2017-11-18 00:30:37 +0100

    Prefer short echo tags

--- views/info.php
+++ views/info.php
@@ -24,8 +24,8 @@
     href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
 </p>
 <div class="moved_syscheck">
-    <h2><?php echo $this->text('syscheck_title')?></h2>
+    <h2><?=$this->text('syscheck_title')?></h2>
 <?php foreach ($checks as $check):?>
-    <p class="xh_<?php echo $check->getState()?>"><?php echo $this->text('syscheck_message', $check->getLabel(), $check->getStateLabel())?></p>
+    <p class="xh_<?=$check->getState()?>"><?=$this->text('syscheck_message', $check->getLabel(), $check->getStateLabel())?></p>
 <?php endforeach?>
 </div>
